3dda565bc6c7e125c5413887dc68aa266dcc2f68
Edgars Avotins <edgars.avotins@testdevlab.com>
2017-12-20 18:37:20 +0200

    fix reporter

--- reporter.rb
+++ reporter.rb
@@ -4,11 +4,11 @@ require_relative 'features/support/api'
 build_number = ARGV[0]
 job_url = ARGV[1]
 
-thumbnail = {'url' => 'http://i0.kym-cdn.com/photos/images/facebook/000/711/605/f6d.jpg'}
+thumbnail = {'url' => 'https://image.spreadshirtmedia.com/image-server/v1/mp/compositions/1020402791/views/1,width=300,height=300,backgroundColor=E8E8E8,version=1498225413/perhaps-cow-meme.jpg'}
 fields = []
-fields.push({'name' => 'a', 'value' => 'b'})
+fields.push({'name' => 'perhaps', 'value' => 'perhaps'})
 embed = []
-embed.push('title' => 'c',
+embed.push('title' => 'perhaps',
   'color' => 2210844,
   'fields' => fields,
   'thumbnail' => thumbnail,
