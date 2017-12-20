afd1ea6a1e55a1e973179b7d486fbf56bffe1358
Edgars Avotins <edgars.avotins@testdevlab.com>
2017-12-20 18:35:00 +0200

    smth

--- reporter.rb
+++ reporter.rb
@@ -1,18 +1,20 @@
 require 'json'
 require_relative 'features/support/api'
 
-thumbnail = {'url' => 'https://media.giphy.com/media/8oh42nM14t50Q/giphy-facebook_s.jpg'}
+build_number = ARGV[0]
+job_url = ARGV[1]
+
+thumbnail = {'url' => 'http://i0.kym-cdn.com/photos/images/facebook/000/711/605/f6d.jpg'}
 fields = []
-fields.push({'name' => 'fieldname', 'value' => 'fieldvalue'})
+fields.push({'name' => 'a', 'value' => 'b'})
 embed = []
-embed.push('title' => 'Rich content',
-  'title' => 'some title',
+embed.push('title' => 'c',
   'color' => 2210844,
   'fields' => fields,
   'thumbnail' => thumbnail,
 )
 
-payload = {'content' => 'magestic dickbutt', 'embeds' => embed}.to_json
+payload = {'content' => "#{build_number}\n #{job_url}", 'embeds' => embed}.to_json
 url = "https://discordapp.com/api/webhooks/393067525451022336/uz2WgUi_8-6oS9zy2Pu_3l_-CtQvabdSlgflF_ojyxTxWgxO_8Vdj0qBDMNixDj6wlT1"
 
 @api = Api.new
